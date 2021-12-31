21941776f9405c53768747f5e49fa920f018954f
Nguyen Huyen <huyencapuchino2512@gmail.com>
2022-01-01 00:19:38 +0700

    Test for cherry-pick

--- src/App.js
+++ src/App.js
@@ -14,16 +14,7 @@ class App extends Component {
         <Person name="Tedu5" age="17">My hobbies: teaching</Person>
         
       </div>
-
-      <div className="App">
-        <h1>This is my first ReactJS app, Please don't ...</h1> 
-        <Person name="Tedu1" age="14"/>
-        <Person name="Tedu2" age="15"/>
-        <Person name="Tedu3" age="16"/>
-        <Person name="Tedu4" age="17">My hobbies: teaching</Person>
-        <Person name="Tedu5" age="17">My hobbies: teaching</Person>
-        
-      </div>
+<h1>No meaning</h1>
        <p>Des</p>
     </>
   );
